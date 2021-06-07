f260b2eb146b8ff82d501df4237cb02e557f7239
Savidya Sathyani <savidya19@gmail.com>
2021-06-07 20:22:42 +0530

    Add layouts, routes and footer

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './styles.scss';
+import { Link } from 'react-router-dom';
+
+// assets
 import LOGO from '../../assets/logo.png';
 
 const Header = props => {
@@ -7,7 +10,19 @@ const Header = props => {
     <header className="header">
       <div className="wrap">
         <div className="logo">
-          <img src={LOGO} alt="SimpleTut" />
+          <Link to="/">
+            <img src={LOGO} alt="SimpleTut" />
+          </Link>
+        </div>
+
+        <div className="callToActions">
+          <ul>
+            <li>
+              <Link to="/registration">
+                Register
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </header>
