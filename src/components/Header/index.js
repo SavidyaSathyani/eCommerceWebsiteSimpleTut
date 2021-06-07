7bed423e34e2f65ae5bf4013f14511060b5baa55
Savidya Sathyani <savidya19@gmail.com>
2021-06-08 00:22:44 +0530

    Configure firebase and add social logins with Google

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './styles.scss';
 import { Link } from 'react-router-dom';
+import { auth } from '../../firebase/utils';
 
 // assets
 import LOGO from '../../assets/logo.png';
 
 const Header = props => {
+
+  const {
+    currentUser,
+  } = props;
+
   return (
     <header className="header">
       <div className="wrap">
@@ -16,17 +22,36 @@ const Header = props => {
         </div>
 
         <div className="callToActions">
-          <ul>
-            <li>
-              <Link to="/registration">
-                Register
-              </Link>
-            </li>
-          </ul>
+
+          {!currentUser
+            ? (<ul>
+              <li>
+                <Link to="/registration">
+                  Register
+                </Link>
+              </li>
+              <li>
+                <Link to="/login">
+                  Login
+                </Link>
+              </li>
+            </ul>)
+            : (<ul>
+              <li>
+                <span onClick={() => auth.signOut()}>
+                  LOGOUT
+                </span>
+              </li>
+            </ul>)
+          }
         </div>
       </div>
     </header>
   )
 };
+
+Header.defaultProp = {
+  currentUser: null,
+}
 
 export default Header;
